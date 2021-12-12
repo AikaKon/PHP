6490cd548f60e3d6caf951dbc6573cafea80e4cd
AikaKon <ika09861@gmail.com>
2021-12-12 17:16:51 +0900

    no message

--- Laravel/MyBBS/resources/views/posts/edit.blade.php
+++ Laravel/MyBBS/resources/views/posts/edit.blade.php
@@ -1,18 +1,19 @@
 <x-layout>
     <x-slot name="title">
-        Add New Post - My BBS
+        Edit Post - My BBS
     </x-slot>
     <div class="back-link">
-        &laquo; <a href="{{ route('posts.index')}}">Back</a>
+        &laquo; <a href="{{ route('posts.show', $post)}}">Back</a>
     </div>
     <h1>Add New Post</h1>
 
-    <form method="post" action="{{ route('posts.store') }}">
+    <form method="post" action="{{ route('posts.update', $post) }}">
+        @method('PATCH')
         @csrf
         <div class="form-group">
             <label>
                 title
-                <input type="text" name="title" value="{{ old('title')}}">
+                <input type="text" name="title" value="{{ old('title', $post->title)}}">
             </label>
             @error('title')
                 <div class="error">{{ $message }}</div>
@@ -21,14 +22,14 @@
         <div class="form-group">
             <label for="">
                 Body
-                <textarea name="body">{{ old('body')}}</textarea>
+                <textarea name="body">{{ old('body', $post->body)}}</textarea>
             </label>
             @error('body')
                 <div class="error">{{ $message }}</div>
             @enderror
         </div>
         <div class="form-button">
-            <button>add</button>
+            <button>update</button>
         </div>
     </form>
 </x-layout>
